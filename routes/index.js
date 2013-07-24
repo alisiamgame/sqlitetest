bf5fd02d6aa50f873a8bbade67d744fafa2d816e
alisia <alisiamgame@gmail.com>
2013-07-24 22:16:24 +0900

    revert iconv

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,4 @@
 
-/*
- * GET home page.
- */
-
 var sqlite3 = require('sqlite3').verbose();
 var Iconv = require('iconv').Iconv;
 var Buffer = require('buffer').Buffer;
@@ -12,31 +8,10 @@ exports.index = function(req, res){
 
 	db.serialize(function() {
 		db.all("select * from log_pc limit 10", function(err, rows) {
-			//res.locals.rows = rows;
-			
-			var transcopy = new Buffer(rows);
-			console.log(transcopy);
-			//var buffer = iconv.convert(transcopy);
-			//console.log(buffer.toJSON());
-			
-			//console.log(res.locals.rows);
-			//res.render('index', { title: 'Express' });
-		});
-		
-		db.each("select * from log_pc limit 10", function(err, row) {
-			var iconv = new Iconv('EUC-KR', 'UTF-8');
-			var buffer = iconv.convert(row.szaccountname);
-			console.log(buffer.toString('utf8'));
-			console.log(buffer.inspect());
-			console.log(row.szaccountname);
-			res.locals.row = row;
-			res.locals.szaccountname = buffer.toString('utf8');
-			
+			res.locals.rows = rows;
 			res.render('index', { title: 'Express' });
 		});
 	});
-	
-	//console.log(iconv.convert(buffer));
 
 	db.close();
 };
